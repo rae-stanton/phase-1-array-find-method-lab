22da8fe69d3c1d831225062308b399d96ade1824
Rae Stanton <raestanton93@gmail.com>
2023-08-01 15:38:41 -0700

    find method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+const winRecord = [
+  { year: 1997, result: 'L' },
+  { year: 1998, result: 'W' },
+  { year: 1999, result: 'L' },
+  { year: 2015, result: 'L' },
+];
+
+const noWins = [
+  { year: 2000, result: "L" },
+  { year: 2001, result: "L" },
+]
+
+
+function superbowlWin(record) {
+  const winRecord = record.find(game => game.result === 'W');
+
+  if (winRecord) {
+    return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
